6ca05ffb919b7480dec44f70972a8faa92a5f9c5
Danielle Martin <139602254+danim53@users.noreply.github.com>
2026-03-16 13:02:28 -0700

    read domain name and root DNS IP then print to test that program runs

--- mydns.py
+++ mydns.py
@@ -1,1 +1,15 @@
+import sys
 
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python3 mydns.py domain-name root-dns-ip")
+        return
+
+    domain_name = sys.argv[1]
+    root_dns_ip = sys.argv[2]
+
+    print("Domain Name: ", domain_name)
+    print("Root DNS IP: ", root_dns_ip)
+
+if __name__ == "__main__":
+    main()
